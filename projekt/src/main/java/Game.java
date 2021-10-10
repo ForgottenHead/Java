2059bab5259984e548102c66c7edf1d05795575a
ForgottenHead <40234212+ForgottenHead@users.noreply.github.com>
2021-10-10 11:31:48 +0200

    Delete projekt directory

--- projekt/src/main/java/Game.java
+++
@@ -1,9 +0,0 @@
-public class Game {
-    int playersCount;
-    Player[] players;
-    Board board;
-
-    static Game createGame(){
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-}
